89f994946de3d2a7a75294ac2285a399e44d4174
Anton Kondratyk <andeev1910@mail.ru>
2022-08-28 22:19:35 +0500

    Исправил замечания.

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -8,7 +8,21 @@ namespace Lab4
 {
     class Program
     {
+        static int Sqr(int val)
+        {
+            int result = 0;
+            for (int i = 1; i <= 2 * val - 1; i = i + 2)
+                result += i;
+            return result;
+        }
         static void Main(string[] args)
+        {
+            int val = 9;
+            for (int i = 1; i <= val; i++)
+                Console.WriteLine(i + ": " + Sqr(i));
+            Console.ReadKey(true);
+        }
+        /*static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
@@ -18,6 +32,6 @@ namespace Lab4
                 Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
             }
             Console.ReadKey();
-        }
+        }*/
     }
 }
